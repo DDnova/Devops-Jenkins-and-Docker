3c467f37c656ab6267f625076d17e4ea8126c222
Darryl Dsouza <82214133+DDnova@users.noreply.github.com>
2023-03-25 09:12:07 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello World, Is everything Fine?');
 });
 
 /* istanbul ignore next */
