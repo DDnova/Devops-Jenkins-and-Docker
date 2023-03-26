0d933081b66144fd94a0138bf5d2b65156013b91
Darryl Dsouza <82214133+DDnova@users.noreply.github.com>
2023-03-26 11:16:32 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('This is a demo of ci cd pipeline');
+  res.send('Hello World 2');
 });
 
 /* istanbul ignore next */
